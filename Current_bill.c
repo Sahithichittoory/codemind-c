3bb33cdfab850dc016401b24b42548c7254497bd
Sahithichittoory <119720163+Sahithichittoory@users.noreply.github.com>
2023-01-02 22:51:28 +0530

    #include<stdio.h> int main() {     float u,c,b;     scanf("%f",&u);     if(u<200)     {         c=u*1.20;     }     else if(u>200&&u<400)     {         c=u*1.50;     }     else if(u>400&&u<600)     {         c=u*1.80;     }     else if(u>=600)     {         c=u*2.00;     }     if(c>400)     {         b=c+c*0.15;     }     else     {         b=c+100;     }     printf("%0.2f",b); }

--- Current_bill.c
+++ Current_bill.c
@@ -1,32 +1,31 @@
 #include<stdio.h>
 int main()
 {
-   float u,c,b,t,s;
-   scanf("%f",&u);
-   if(u<=199)
-   {
-       c=1.20;
-   }
-   else if(u>=200&&u<400)
-   {
-       c=1.50;
-   }
-   else if(u>=400&&u<600)
-   {
-       c=1.80;
-   }
-   else 
-   {
-       c=2.00;
-   }
- 
-   b=c*u;
-   if(b>400)
-   t=b+b*0.15;
-   else
-   
-   t=b+100;
-   {
-       printf("%0.2f",t);
-   }
+    float u,c,b;
+    scanf("%f",&u);
+    if(u<200)
+    {
+        c=u*1.20;
+    }
+    else if(u>200&&u<400)
+    {
+        c=u*1.50;
+    }
+    else if(u>400&&u<600)
+    {
+        c=u*1.80;
+    }
+    else if(u>=600)
+    {
+        c=u*2.00;
+    }
+    if(c>400)
+    {
+        b=c+c*0.15;
+    }
+    else
+    {
+        b=c+100;
+    }
+    printf("%0.2f",b);
 }
